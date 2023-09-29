426010160255e535937b2bbb0ea3820fd2c6b71f
Jalish Mahmud <jalish@shellbeehaken.com>
2023-09-29 17:36:17 +0600

    dark and light theme toggle done

--- src/app/components/theme-toggle/ThemeToggle.js
+++ src/app/components/theme-toggle/ThemeToggle.js
@@ -1,7 +1,34 @@
+"use client";
+import Image from "next/image";
 import styles from "./ThemeToggle.module.css";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
-  return <div className={styles["container"]}>ThemeToggle</div>;
+  const { theme, toggle } = useContext(ThemeContext);
+
+  return (
+    <div
+      onClick={toggle}
+      className={styles["container"]}
+      style={
+        theme === "dark"
+          ? { backgroundColor: "white" }
+          : { backgroundColor: "#0f172a" }
+      }
+    >
+      <Image src="/moon.png" alt="moon" width={14} height={14} />
+      <div
+        className={styles["ball"]}
+        style={
+          theme === "dark"
+            ? { left: 1, background: "#0f172a" }
+            : { right: 1, background: "white" }
+        }
+      ></div>
+      <Image src="/sun.png" alt="sun" width={14} height={14} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
