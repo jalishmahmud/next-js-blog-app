bc075fd3bed7d0b167b3ab964bef021c1757444e
Jalish Mahmud <jalish@shellbeehaken.com>
2023-09-29 13:07:16 +0600

    component structure create

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,34 +1,21 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Featured from "./components/featured/Featured";
+import CategoryList from "./components/category-list/CategoryList";
+import CardList from "./components/card-list/CardList";
+import Menu from "./components/menu/Menu";
 
 export default function Home() {
   return (
-    <Link href="/">
-      <h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ea
-        nostrum at qui. Voluptatibus maxime consequatur numquam recusandae natus
-        dolor at cupiditate, eveniet quibusdam mollitia sunt et vero dolorem
-        minus, nobis architecto temporibus adipisci deleniti. Pariatur debitis
-      </h2>
-      sint neque, vero fugit accusantium unde necessitatibus officia culpa at
-      doloribus incidunt deleniti nemo omnis veniam adipisci vel. Ratione, culpa
-      sint asperiores ipsa nam, recusandae obcaecati corrupti omnis in ullam
-      maxime quisquam cum, totam consequatur illum suscipit quibusdam
-      voluptatum. Quo odio nesciunt magnam ipsa sequi autem vero numquam
-      corrupti officiis expedita doloribus quaerat voluptates amet nam
-      perferendis dolorum, eum quibusdam dolor itaque deserunt consequuntur esse
-      hic optio earum. Optio assumenda eveniet rerum pariatur mollitia debitis
-      obcaecati dolorum? Dolorem voluptatibus, facere, inventore quia
-      blanditiis, recusandae porro iure consequuntur quis commodi fuga
-      distinctio nihil voluptatum facilis voluptate. Repellat debitis nobis
-      temporibus error nisi hic voluptatem blanditiis vel corporis totam
-      veritatis facilis, quos officiis. Voluptatum odio officiis enim labore!
-      Blanditiis ex tempora voluptates officiis rerum labore earum nam, iure
-      dolore error. Totam atque deserunt eum doloribus eligendi sapiente
-      suscipit velit doloremque quae tempora. Consequatur quod velit nobis
-      architecto ipsam quis, officia inventore corporis perferendis atque
-      aperiam accusamus qui temporibus ab beatae doloribus nesciunt cum tempora
-      cumque.
-    </Link>
+    <div className={styles["container"]}>
+      <Featured />
+      <CategoryList />
+      <div className={styles["content"]}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
   );
 }
